3102c56bab62dc40e6c8f6639a2f19d2f830e3c2
druizz90 <32929872+druizz90@users.noreply.github.com>
2022-06-06 16:10:03 +0200

    Fix warnings caused by pytest configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 from typing import AsyncGenerator
 
-import pytest
+import pytest_asyncio
 from asgi_lifespan import LifespanManager
 from httpx import AsyncClient
 
 from app.main import app
 
 
-@pytest.fixture()
+@pytest_asyncio.fixture()
 async def client() -> AsyncGenerator[AsyncClient, None]:
     async with LifespanManager(app):
         async with AsyncClient(app=app, base_url="http://test") as ac:
